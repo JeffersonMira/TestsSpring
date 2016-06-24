21b3b9e90b4360d40a12f916e2703b740fc7c52c
Jefferson S Mira <projeto004@scopus.com.br>
2016-06-24 10:06:57 -0300

    Code for JDBCTemplate

--- src/main/java/jeff/study/tests/TestesSpring/DBAccess/App.java
+++ src/main/java/jeff/study/tests/TestesSpring/DBAccess/App.java
@@ -9,24 +9,48 @@ public class App {
 
 	public static void main(String[] args) {
 
-		 ApplicationContext ap =  new ClassPathXmlApplicationContext("file:src/main/java/jeff/study/tests/TestesSpring/DBAccess/springconfgs/spring-beans.xml");
-    	 
-		 PessoaDAO pDAO = (PessoaDAO)ap.getBean("pessoaDAO");
-		 Pessoa p = new Pessoa();
-		 p.setName("João");
-		 p.setPhone(12345L);
-		 pDAO.inserir(p);
-		 List<Pessoa> ps =  pDAO.findAll();
+//		 ApplicationContext ap =  new ClassPathXmlApplicationContext("file:src/main/java/jeff/study/tests/TestesSpring/DBAccess/springconfgs/spring-beans.xml");
+//    	 
+//		 PessoaDAO pDAO = (PessoaDAO)ap.getBean("pessoaDAO");
+//		 Pessoa p = new Pessoa();
+//		 p.setName("João");
+//		 p.setPhone(12345L);
+//		 pDAO.inserir(p);
+//		 List<Pessoa> ps =  pDAO.findAll();
 		 
 		 
-//		 ApplicationContext ap =  new ClassPathXmlApplicationContext("file:src/main/java/jeff/study/tests/TestesSpring/DBAccess/springconfgs/spring-beans-jdbctemplate.xml");
-//		 PessoaDAOJDBCTemplate p = (PessoaDAOJDBCTemplate) ap.getBean("pessoaDAOJDBCTemplate");
-//		 
-//		 List<Pessoa> ps = p.findAll();
+		 ApplicationContext ap =  new ClassPathXmlApplicationContext("file:src/main/java/jeff/study/tests/TestesSpring/DBAccess/springconfgs/spring-beans-jdbctemplate.xml");
+		 PessoaDAOJDBCTemplate p = (PessoaDAOJDBCTemplate) ap.getBean("pessoaDAOJDBCTemplate");
+		 
+		 System.out.println("\n------------  Test 1 --------------------");
+		 List<Pessoa> ps = p.findAll();
 		 
 		 for (Pessoa pessoa : ps) {
 			System.out.println(pessoa.toString());
 		}
+		 
+		 System.out.println("\n------------  Test 2 --------------------");
+		 
+		 Pessoa pChanged = ps.get(ps.size()-1);
+		 pChanged.setName("Amaterasu");
+		 p.update(pChanged);
+		 
+		 ps = p.findAll();
+		 
+		 for (Pessoa pessoa : ps) {
+			System.out.println(pessoa.toString());
+		}
+		 
+		System.out.println("\n------------  Test 3 --------------------");
+		 
+		 Pessoa pOther = new Pessoa();
+		 pOther.setName("Madara");
+		 pOther.setPhone(12347L);
+		 Long id = p.Insert(pOther);
+		 System.out.println("Id Inserted is: "+id);
+		 
+		 
+		 
 	}
 
 }
--- src/main/java/jeff/study/tests/TestesSpring/DBAccess/PessoaDAOJDBCTemplate.java
+++ src/main/java/jeff/study/tests/TestesSpring/DBAccess/PessoaDAOJDBCTemplate.java
@@ -3,26 +3,27 @@ package jeff.study.tests.TestesSpring.DBAccess;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
+/**
+ * Também é possível fazer o processamento em lote utilizando o BatchPreparedStatementSetter
+ * */
 public class PessoaDAOJDBCTemplate {
 
 	private JdbcTemplate jdbcTemplate;
 	private Connection con;
-	
-	public void inserir(Pessoa p){
-		
-	}
-	
+	/*
+	 * Seria possível também criar uma classe que implementasse o mapRow, que inclusive eu acho ser 
+	 * o melhor a ser feito. Mas apenas para fins de estudo, está valendo.
+	 * */
 	public List<Pessoa> findAll(){
-
 		return jdbcTemplate.query("select * from pessoa", 
-
 				new RowMapper<Pessoa>(){
-
 			public Pessoa mapRow(ResultSet rs, int rownum) throws SQLException {
 				return new Pessoa(
 						rs.getInt(1),
@@ -31,10 +32,42 @@ public class PessoaDAOJDBCTemplate {
 			}
 
 		});
-		
 	}
 	
-
+	/*O update tbm é bem simples*/
+	public void update(Pessoa pessoa) {
+		getJdbcTemplate().update("update pessoa set nome = ? where id = ?",
+				pessoa.getName(), pessoa.getId());
+	}
+	
+	/*
+	 * Este 'Execute' pode ser utilizado para executar qualquer comando SQL.
+	 * Inclusive DDL.*/
+	public void createTable(){
+		
+		String ddl = "create table assunto(" +
+				"id long auto_increment not null, " +
+				"nome varchar(255) not null, " +
+				"primary key(id))";
+				getJdbcTemplate().execute(ddl);
+	}
+	
+	public Long Insert(Pessoa p){
+		HashMap<String, Object> parameters = new HashMap<String, Object>();
+		
+		parameters.put("nome", p.getName());
+		parameters.put("telefone", p.getPhone());
+		
+		/*
+		 * Utiliza o design pattern 'builder' e já retorna o valor gerado automágicamente.*/
+		Long id = new SimpleJdbcInsert(getJdbcTemplate())
+				.withTableName("pessoa")
+				.usingGeneratedKeyColumns("id")
+				.executeAndReturnKey(parameters).longValue();
+		
+		return id;
+	}
+	
 	public JdbcTemplate getJdbcTemplate() {
 		return jdbcTemplate;
 	}
